9633be05a67bcbc9897be7dab172a2c5b9218f3e
Lucas Santos Rodrigues <lucas.rodrigues2@fit-tecnologia.org.br>
2022-05-12 10:18:12 -0300

    adding model validation, tag [required] in name and isGlutenFree in Pizza Model

--- WebApplication2/Models/Pizza.cs
+++ WebApplication2/Models/Pizza.cs
@@ -1,8 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApplication2.Models;
 
 public class Pizza
 {
+    [Required]
     public int id { get; set; }
+
+    [Required]
     public string? name { get; set; }
+
+    [Required]
     public bool isGlutenFree { get; set; }
+
 }
